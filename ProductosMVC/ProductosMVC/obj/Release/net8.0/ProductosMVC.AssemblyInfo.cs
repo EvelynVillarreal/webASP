ee3a18d1324c72cae7501e1eb702bdfa4088ab8d
EvelynVillarreal <ehvillarreal@espe.edu.ec>
2026-05-20 14:39:03 -0500

    fix css

--- ProductosMVC/ProductosMVC/obj/Release/net8.0/ProductosMVC.AssemblyInfo.cs
+++ ProductosMVC/ProductosMVC/obj/Release/net8.0/ProductosMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductosMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+799552176d14b5f91bea322c36f3017d4f816f98")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductosMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductosMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
